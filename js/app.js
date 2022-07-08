90ccaa653523029916b31f1a437c788a194cba4a
GovindProMax <hargovind.bling@gmail.com>
2022-07-08 14:04:25 -0600

    tiles completed

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 console.log("I am working");
+noteCards();
 
 // Working on the add note button
 let addBtn = document.getElementById('addBtn');
 
 //Add an event listner
-addBtn.addEventListener("click",function(e){
+addBtn.addEventListener("click", function (e) {
 
     //Working on the input text area 
     let addTxt = document.getElementById('addTxt');
@@ -13,47 +14,51 @@ addBtn.addEventListener("click",function(e){
     let currentNotes = localStorage.getItem("currentNotes");
 
     //Clear the notesObj is nothing inside local storage
-    if(currentNotes == null)
-    {
+    if (currentNotes == null) {
         notesObj = [];
     }
-    else{
+    else {
         notesObj = JSON.parse(currentNotes);
     }
 
     //Add the value inside text area to the notesObj array
     notesObj.push(addTxt.value);
     //Update the local storage
-    localStorage.setItem("currentNotes",JSON.stringify(notesObj));
+    localStorage.setItem("currentNotes", JSON.stringify(notesObj));
     //Reset the value of the text area
-    addTxt.value="";
-    
+    addTxt.value = "";
+
     noteCards();
 
 })
 
-function noteCards(){
+function noteCards() {
     let currentNotes = localStorage.getItem("currentNotes");
     //Clear the notesObj is nothing inside local storage
-    if(currentNotes == null)
-    {
+    if (currentNotes == null) {
         notesObj = [];
     }
-    else{
+    else {
         notesObj = JSON.parse(currentNotes);
     }
-    let myHTML=""
+    let myHTML = ""
 
-    currentNotes.forEach(function(element,index){
+    notesObj.forEach(function (element, index) {
         myHTML += `
         <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
                 <div class="card-body">
-                    <h5 class="card-title">Note ${index + 1 } </h5>
+                    <h5 class="card-title">Note ${index + 1} </h5>
                     <p class="card-text">${element}</p>
                     <a href="#" class="btn btn-primary">Delete Note</a>
                 </div>
             </div>`;
     });
 
-    
+    let notesElements = document.getElementById('notes');
+
+    if (notesElements.length != 0) {
+        notesElements.innerHTML = myHTML;
+    }
+
+
 }
