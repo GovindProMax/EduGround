789ec09ab4c241f5fe3b4b2cf1248e72a596081c
GovindProMax <hargovind.bling@gmail.com>
2022-07-16 13:11:29 -0600

    add cards

--- js/news.js
+++ js/news.js
@@ -9,7 +9,7 @@ let newsAccordion = document.getElementById('newsAccordion');
 
 // Create an ajax get request
 const xhr = new XMLHttpRequest();
-xhr.open('GET', `https://newsapi.org/v2/everything?q=musk&country=us&apiKey=${apiKey}`, true);
+xhr.open('GET', `https://newsapi.org/v2/everything?q=trump&apiKey=${apiKey}`, true);
 
 // What to do when response is ready
 xhr.onload = function () {
@@ -18,22 +18,20 @@ xhr.onload = function () {
         let articles = json.articles;
         console.log(articles);
         let newsHtml = "";
-        articles.forEach(function(element, index) {
+        articles.forEach(function (element, index) {
             // console.log(element, index)
-            let news = `<div class="card">
-                            <div class="card-header" id="heading${index}">
-                                <h2 class="mb-0">
-                                <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse${index}"
-                                    aria-expanded="false" aria-controls="collapse${index}">
-                                   <b>Breaking News ${index+1}:</b> ${element["title"]}
-                                </button>
-                                </h2>
+            let news = `<div class="col-sm-4">
+                                <div class="card h-100">
+                                    <div class="card-body">
+                                        <img class="card-img-top" src="${element["urlToImage"]}" alt="Card image cap">
+                                        <div class="card-body">
+                                        <h5 class="card-title">${element["title"]}</h5>
+                                        <p class="card-text">${element["content"]}</p>
+                                        <a href="${element['url']}" target="_blank" class="btn btn-primary">Read More</a>
+                                    </div>
+                                </div>
                             </div>
-
-                            <div id="collapse${index}" class="collapse" aria-labelledby="heading${index}" data-parent="#newsAccordion">
-                                <div class="card-body"> ${element["content"]}. <a href="${element['url']}" target="_blank" >Read more here</a>  </div>
-                            </div>
-                        </div>`;
+                        </div>`
             newsHtml += news;
         });
         newsAccordion.innerHTML = newsHtml;
