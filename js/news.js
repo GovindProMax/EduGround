f97249e81aae08cf2efd96a80d6cd6c804d5bfa6
GovindProMax <hargovind.bling@gmail.com>
2022-07-18 16:54:01 -0600

    css added to news and dropdown search #1 functional

--- js/news.js
+++ js/news.js
@@ -1,87 +1,86 @@
-console.log("This is my index js file");
-
-// Initialize the news api parameters
-let source = 'the-times-of-india';
-let apiKey = 'f81e0c84fc5f454a830949aac7fdac51'
-
-// Grab the news container
-let newsAccordion = document.getElementById('newsAccordion');
-
-/**
- * Code to filter the dropdown
- * 
- */
-//Initialize with the list of symbols
-let names = ["All Universities","York University","University of Alberta"]
-
-//Find the input search box
-let search = document.getElementById("searchUniversity")
-
-//Find every item inside the dropdown
-let items = document.getElementsByClassName("dropdown-item")
-function buildDropDown(values) {
-    let contents = []
-    for (let name of values) {
-    contents.push('<input type="button" class="dropdown-item" type="button" value="' + name + '"/>')
-    }
-    $('#menuItems').append(contents.join(""))
-
-    //Hide the row that shows no items were found
-    $('#empty').hide()
+function searchTextFunction() {
+    let searchText = document.getElementById('searchBar').value;
+    console.log(searchText)
+    searchWhere = document.getElementById('searchEverywhereorTopHeadlines').innerText.trim();
+    console.log(searchWhere)
+    apiCall(searchWhere, searchText)
 }
 
-//Capture the event when user types into the search box
-window.addEventListener('input', function () {
-    filter(search.value.trim().toLowerCase())
-})
+function searchEverywhereOrTopHeadlines() {
+    //Initialize with the list of symbols
+    let names = ["Top-Headlines","Everything"]
 
-//For every word entered by the user, check if the symbol starts with that word
-//If it does show the symbol, else hide it
-function filter(word) {
-    let length = items.length
-    let collection = []
-    let hidden = 0
-    for (let i = 0; i < length; i++) {
-    if (items[i].value.toLowerCase().startsWith(word)) {
-        $(items[i]).show()
-    }
-    else {
-        $(items[i]).hide()
-        hidden++
-    }
+    //Find the input search box
+    let search = document.getElementById("searchEverywhereOrHeadlines")
+
+    //Find every item inside the dropdown
+    let items = document.getElementsByClassName("dropdown-item")
+    function buildDropDown(values) {
+        let contents = []
+        for (let name of values) {
+            contents.push('<input type="button" class="dropdown-item" type="button" value="' + name + '"/>')
+        }
+        $('#menuItems').append(contents.join(""))
+
+        //Hide the row that shows no items were found
+        $('#empty').hide()
     }
 
-    //If all items are hidden, show the empty view
-    if (hidden === length) {
-    $('#empty').show()
+    //Capture the event when user types into the search box
+    window.addEventListener('input', function () {
+        filter(search.value.trim().toLowerCase())
+    })
+
+    //For every word entered by the user, check if the symbol starts with that word
+    //If it does show the symbol, else hide it
+    function filter(word) {
+        let length = items.length
+        let collection = []
+        let hidden = 0
+        for (let i = 0; i < length; i++) {
+            if (items[i].value.toLowerCase().startsWith(word)) {
+                $(items[i]).show()
+            }
+            else {
+                $(items[i]).hide()
+                hidden++
+            }
+        }
+
+        //If all items are hidden, show the empty view
+        if (hidden === length) {
+            $('#empty').show()
+        }
+        else {
+            $('#empty').hide()
+        }
     }
-    else {
-    $('#empty').hide()
-    }
+
+    //If the user clicks on any item, set the title of the button as the text of the item
+    $('#menuItems').on('click', '.dropdown-item', function () {
+        $('#searchEverywhereorTopHeadlines').text($(this)[0].value);
+    })
+
+    buildDropDown(names)
 }
 
-//If the user clicks on any item, set the title of the button as the text of the item
-$('#menuItems').on('click', '.dropdown-item', function(){
-    $('#dropdown_coins').text($(this)[0].value)
-    $("#dropdown_coins").dropdown('toggle');
-})
+function apiCall(searchWhere, searchText) {
+    const apiKey = 'f81e0c84fc5f454a830949aac7fdac51'
+    const xhr = new XMLHttpRequest();
+    let newsAccordion = document.getElementById('newsAccordion');
+    xhr.open('GET', `https://newsapi.org/v2/${searchWhere}?q=${searchText}&apiKey=${apiKey}`, true);
 
-buildDropDown(names)
-
-// Create an ajax get request
-const xhr = new XMLHttpRequest();
-xhr.open('GET', `https://newsapi.org/v2/everything?q=lakehead%20university&apiKey=${apiKey}`, true);
-
-// What to do when response is ready
-xhr.onload = function () {
-    if (this.status === 200) {
-        let json = JSON.parse(this.responseText);
-        let articles = json.articles;
-        console.log(articles);
-        let newsHtml = "";
-        articles.forEach(function (element, index) {
-            // console.log(element, index)
-            let news = `<div class="col-sm-4 mb-4">
+    // What to do when response is ready
+    xhr.onload = function () {
+        if (this.status === 200) {
+            let json = JSON.parse(this.responseText);
+            let articles = json.articles;
+            console.log(articles);
+            let newsHtml = "";
+            articles.forEach(function (element, index) {
+                if (element["content"] != null) {
+                    // console.log(element, index)
+                    let news = `<div class="col-sm-4 mb-4">
                                 <div class="card h-100 ">
                                     <div class="card-body">
                                         <img class="card-img-top" src="${element["urlToImage"]}" alt="Card image cap">
@@ -93,13 +92,18 @@ xhr.onload = function () {
                                 </div>
                             </div>
                         </div>`
-            newsHtml += news;
-        });
-        newsAccordion.innerHTML = newsHtml;
+                    newsHtml += news;
+                }
+            });
+            newsAccordion.innerHTML = newsHtml;
+        }
+        else {
+            console.log("Some error occured")
+        }
     }
-    else {
-        console.log("Some error occured")
-    }
+
+    xhr.send()
 }
 
-xhr.send()
+searchEverywhereOrTopHeadlines()
+apiCall('top-headlines', 'canada')
