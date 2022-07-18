294d4b32b88ad007ff208697d44f43429a12320a
GovindProMax <hargovind.bling@gmail.com>
2022-07-18 13:36:54 -0600

    postman clone files

--- js/news.js
+++ js/news.js
@@ -7,9 +7,70 @@ let apiKey = 'f81e0c84fc5f454a830949aac7fdac51'
 // Grab the news container
 let newsAccordion = document.getElementById('newsAccordion');
 
+/**
+ * Code to filter the dropdown
+ * 
+ */
+//Initialize with the list of symbols
+let names = ["All Universities","York University","University of Alberta"]
+
+//Find the input search box
+let search = document.getElementById("searchUniversity")
+
+//Find every item inside the dropdown
+let items = document.getElementsByClassName("dropdown-item")
+function buildDropDown(values) {
+    let contents = []
+    for (let name of values) {
+    contents.push('<input type="button" class="dropdown-item" type="button" value="' + name + '"/>')
+    }
+    $('#menuItems').append(contents.join(""))
+
+    //Hide the row that shows no items were found
+    $('#empty').hide()
+}
+
+//Capture the event when user types into the search box
+window.addEventListener('input', function () {
+    filter(search.value.trim().toLowerCase())
+})
+
+//For every word entered by the user, check if the symbol starts with that word
+//If it does show the symbol, else hide it
+function filter(word) {
+    let length = items.length
+    let collection = []
+    let hidden = 0
+    for (let i = 0; i < length; i++) {
+    if (items[i].value.toLowerCase().startsWith(word)) {
+        $(items[i]).show()
+    }
+    else {
+        $(items[i]).hide()
+        hidden++
+    }
+    }
+
+    //If all items are hidden, show the empty view
+    if (hidden === length) {
+    $('#empty').show()
+    }
+    else {
+    $('#empty').hide()
+    }
+}
+
+//If the user clicks on any item, set the title of the button as the text of the item
+$('#menuItems').on('click', '.dropdown-item', function(){
+    $('#dropdown_coins').text($(this)[0].value)
+    $("#dropdown_coins").dropdown('toggle');
+})
+
+buildDropDown(names)
+
 // Create an ajax get request
 const xhr = new XMLHttpRequest();
-xhr.open('GET', `https://newsapi.org/v2/everything?q=trump&apiKey=${apiKey}`, true);
+xhr.open('GET', `https://newsapi.org/v2/everything?q=lakehead%20university&apiKey=${apiKey}`, true);
 
 // What to do when response is ready
 xhr.onload = function () {
@@ -20,8 +81,8 @@ xhr.onload = function () {
         let newsHtml = "";
         articles.forEach(function (element, index) {
             // console.log(element, index)
-            let news = `<div class="col-sm-4">
-                                <div class="card h-100">
+            let news = `<div class="col-sm-4 mb-4">
+                                <div class="card h-100 ">
                                     <div class="card-body">
                                         <img class="card-img-top" src="${element["urlToImage"]}" alt="Card image cap">
                                         <div class="card-body">
